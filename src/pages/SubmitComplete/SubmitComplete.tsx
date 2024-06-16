237a053fb11cad60bc5b3b21ecf4c7d51c447c32
sylee <sylee6529@gmail.com>
2024-06-16 20:22:36 +0900

    Feat: 제출 완료 페이지 컴포넌트화 (#8)

--- src/pages/SubmitComplete/SubmitComplete.tsx
+++ src/pages/SubmitComplete/SubmitComplete.tsx
@@ -4,6 +4,7 @@ import Navbar from "../../components/Navbar/Navbar";
 import Button from "../../components/ui/Button";
 import Container from "../../components/ui/Container";
 import { FaCheck } from "react-icons/fa";
+import SubmitOK from "../../components/ui/SubmitOK";
 
 const SubmitComplete = () => {
   const navigate = useNavigate();
@@ -12,31 +13,13 @@ const SubmitComplete = () => {
     <Container hasHeader={true} full={true}>
       <Header text="불편 도로 제보" closeButton={true} />
 
-      <div
-        className="flex flex-col items-center justify-center h-full text-center px-10 flex gap-2"
-        style={{ height: "calc(100vh - 180px)" }}
-      >
-        <FaCheck size="100" color="3F51B5" />
-        <p className="text-[#404040] font-bold text-xl">제보 감사합니다</p>
-        <div>
-          <p className="text-[#757575] text-sm">
-            관리자 확인 후 승인 처리가 완료되면,
-          </p>
-          <p className="text-[#757575] text-sm">지도에 반영됩니다.</p>
-        </div>
-
-        <div className="absolute bottom-0 left-0 right-0 flex flex-col items-center justify-center mb-4 text-center px-[10px]">
-          <div className="mb-3">
-            <p>승인 상태는 &#39;마이페이지{">"}내가 남긴 제보&#39;에서 </p>
-            <p>확인 가능합니다.</p>
-          </div>
-          <Button
-            text="지도로 돌아가기"
-            size="full"
-            onClick={() => navigate("/")}
-          />
-        </div>
-      </div>
+      <SubmitOK
+        thankYouText="제보 감사합니다"
+        subText="관리자 확인 후 승인 처리가 완료되면,<br /> 지도에 반영됩니다."
+        bottomText="승인 상태는 &#39;마이페이지{'>'}내가 남긴 제보&#39;에서 <br />확인 가능합니다."
+        buttonText="지도로 돌아가기"
+        navigateTo={() => navigate("/")}
+      />
     </Container>
   );
 };
