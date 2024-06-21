0530116ae37a8300b2dba10dba848faf1be8632e
sylee <sylee6529@gmail.com>
2024-06-21 16:02:19 +0900

    Fix: 내가 남긴 제보 -> 나의 제보로 문구 변경 (#8)

--- src/pages/SubmitComplete/SubmitComplete.tsx
+++ src/pages/SubmitComplete/SubmitComplete.tsx
@@ -16,7 +16,7 @@ const SubmitComplete = () => {
       <SubmitOK
         thankYouText="제보 감사합니다"
         subText="관리자 확인 후 승인 처리가 완료되면,<br /> 지도에 반영됩니다."
-        bottomText="승인 상태는 &#39;마이페이지{'>'}내가 남긴 제보&#39;에서 <br />확인 가능합니다."
+        bottomText="승인 상태는 &#39;마이페이지>나의 제보&#39;에서 <br />확인 가능합니다."
         buttonText="지도로 돌아가기"
         navigateTo={() => navigate("/")}
       />
