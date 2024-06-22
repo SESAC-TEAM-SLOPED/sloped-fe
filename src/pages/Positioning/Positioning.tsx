c9c4904f2d8e219553922803b3937e4479444eb3
nhj <nhj1209526@naver.com>
2024-06-22 21:09:17 +0900

    Fix: 중복 코드 수정 (#32)

--- src/pages/Positioning/Positioning.tsx
+++ src/pages/Positioning/Positioning.tsx
@@ -10,7 +10,6 @@ import { useLocation, useNavigate } from "react-router-dom";
 const Positioning = () => {
   const navigate = useNavigate();
   const { pathname } = useLocation();
-  const navigate = useNavigate();
 
   const { location } = useGeoLocation();
   const [address, setAddress] = useState("");
