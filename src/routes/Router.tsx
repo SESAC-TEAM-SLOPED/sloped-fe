49694e4b17c14e940792b3c2c265b000b666e8e0
ChangHoSe <sas1397@naver.com>
2024-06-25 15:12:29 +0900

    Chore : dev의 sidebar 수정사항 병합 (#70)

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -48,9 +48,9 @@ const Router = () => {
         <Route path="/mypage/edit-review" element={<MyReviewEditor />} />
         <Route path="/mypage/my-reports" element={<MyReport />} />
         <Route path="/admin" element={<AdminPageLogin />} />
-        <Route path="/admin/base" element={<AdminPage />} />
         <Route path="/admin" element={<AdminLayout />}>
           <Route path="/admin/reports/facility" element={<AdminReport />} />
+          <Route path="/admin/base" element={<AdminPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
