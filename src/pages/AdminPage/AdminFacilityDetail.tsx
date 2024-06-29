1f1badee39a2ab495fe8dc6f2c3f80dd2166b83b
ChangHoSe <sas1397@naver.com>
2024-06-29 15:06:05 +0900

    Chore : 시설 상세페이지용 임시데이터 수정 (#70)

--- src/pages/AdminPage/AdminFacilityDetail.tsx
+++ src/pages/AdminPage/AdminFacilityDetail.tsx
@@ -9,7 +9,8 @@ const tempFacility = [
     address: "서울시 영등포구 문래동",
     phoneNumber: "02-123-1234",
     accessibilityFeatures: "O, O, X",
-    openingHours: "월 9:00 - 22:00",
+    openingHours:
+      "월 9:00 - 22:00, 화 9:00 - 22:00, 수 9:00 - 22:00, 목 9:00 - 22:00, 금 9:00 - 22:00, 토 9:00 - 22:00, 일 9:00 - 22:00",
     createdAt: "2024-06-06T12:45:21Z",
     modifiedAt: "2024-06-07T10:41:21Z",
   },
