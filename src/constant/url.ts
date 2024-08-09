682e66bcad38ba4fc26d3ee213e7049ac3004885
sylee <sylee6529@gmail.com>
2024-08-09 23:45:12 +0900

    Fix: 배포 url 변경

--- src/constant/url.ts
+++ src/constant/url.ts
@@ -2,7 +2,7 @@ type Environment = "development" | "production";
 
 export const API_URL: Record<Environment, string> = {
   development: "http://localhost:8080",
-  production: "http://api.togetheroad.me:8080",
+  production: "https://api.togetheroad.me",
 };
 
 export const serverUrl =
