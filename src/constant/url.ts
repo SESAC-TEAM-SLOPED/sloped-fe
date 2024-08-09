345982275729f418b1759a5ec00bb46aa9cebc73
sylee <sylee6529@gmail.com>
2024-08-09 20:03:07 +0900

    Feat: dev prod 환경 분리

--- src/constant/url.ts
+++ src/constant/url.ts
@@ -1,5 +1,9 @@
-export const API_URL = {
-  dev: "http://localhost:8080",
+type Environment = "development" | "production";
+
+export const API_URL: Record<Environment, string> = {
+  development: "http://localhost:8080",
+  production: "http://api.togetheroad.me:8080",
 };
 
-export const serverUrl = API_URL.dev;
+export const serverUrl =
+  API_URL[(process.env.REACT_APP_ENV as Environment) || "development"];
